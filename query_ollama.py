18db69bf215afff3ead8c475be224cfba3665f15
Femto-0 <arits.king74@gmail.com>
2025-07-27 15:26:17 -0500

    corrected the function logic

--- query_ollama.py
+++ query_ollama.py
@@ -1,25 +1,27 @@
 import requests
+import json
 
 ollama_URL = "http://localhost:11434/api/generate"
-prompt = "process the given string and return a json response such in the format: <first_name>, <last_name>, <name> and <organization_name>"
 
 
-def query_ollama(prompt, model="llama3.2"):
-    details = []
+def query_ollama(image_to_String, model):
+    prompt = f"""
+Extract relevant information from the following text and return a JSON array of objects with the following fields: first_name, last_name, name(combine first and last name), and organization_name.
+
+Text:
+{image_to_String}
+
+Respond only with the JSON containing the response.
+"""
     response = requests.post(ollama_URL, json={
         "model": model,
         "prompt": prompt,
         "stream": False
     })
     if response.status_code == 200:
-        for item in response:
-            dict = {
-                'first_name': item['first_name'],
-                'last_name': item['last_name'],
-                'name': item['name'],
-                'organization_name': item['organization_name']
-            }
-            details.append(dict)
-        return details
+        data = json.loads(response.text)['response']
+        parsed_data = json.loads(data)
+        print(parsed_data)
+        return parsed_data
     else:
-        print("faulty req to" + {model})
+        print("faulty req to" + model)
