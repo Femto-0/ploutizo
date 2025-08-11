b29dc98fe046672560b4bbe04de9bf78112d9335
Femto-0 <arits.king74@gmail.com>
2025-08-10 21:19:48 -0500

    updated the flow of methods to match the function in cleanEnrichedData.py module

--- source/main.py
+++ source/main.py
@@ -6,12 +6,18 @@ model = "llama3.2"
 
 
 def main():
-    # write the name of your image here without the extension. For example: linkedInImage, image fromGoogle, etc.
+    # write the name of your image here without the extension. For example: for "imageA.jpg", just write "imageA"
     image = 'IMG_5293'
     imageInfo = pi.extract_text_from_image('images/'+image+'.PNG')
     ollama_response = qo.query_ollama(imageInfo, model)
-    data = qa.query_apollo(ollama_response)
-    ced.cleanData(data, image)
+    # get raw data from ollama
+    raw_data = qa.query_apollo(ollama_response)
+    # clean the raw data and filter out the info that we want.
+    # As of now, the filter data contains:
+    # ['first_name','last_name','linkedin_url','organization_name','title','email','last_job','last_job_title']
+    clean_data = ced.extract_relevant_data(raw_data)
+    # save the clean data in a json and xlsx file
+    ced.cleanData(clean_data, image)
 
 
 if __name__ == "__main__":
