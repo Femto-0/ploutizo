ca2737af0b69ba02639641f9cc489d95a0e949c6
Femto-0 <arits.king74@gmail.com>
2025-08-10 21:18:30 -0500

    udpated the function such that the clean data also gets saved in a xlsx file

--- source/cleanEnrichedData.py
+++ source/cleanEnrichedData.py
@@ -1,5 +1,6 @@
 import json
-# import pandas as pd
+import pandas as pd
+from pandas import json_normalize
 
 
 def extract_relevant_data(raw_data):
@@ -16,20 +17,25 @@ def extract_relevant_data(raw_data):
         title = item.get("title")
         email = item.get("email")
 
+        # Employment history
         employment_history = item.get("employment_history", [])
-        if employment_history:
-            most_recent_job = employment_history[0]
-            last_job = employment_history[1]
+        filtered_employment_history = []
+
+        if employment_history and isinstance(employment_history, list):
+            most_recent_job = employment_history[0] if len(
+                employment_history) > 0 else {}
+            last_job = employment_history[1] if len(
+                employment_history) > 1 else {}
+
             job_info = {
                 "organization_name": most_recent_job.get("organization_name"),
-                "most_recent_title": most_recent_job.get("title"),
                 "last_job": last_job.get("organization_name"),
                 "last_job_title": last_job.get("title")
             }
-            filtered_employment_history = [job_info]
-        else:
-            filtered_employment_history = []
 
+            filtered_employment_history.append(job_info)
+
+        # Add to cleaned data
         filtered_detail = {
             "first_name": first_name,
             "last_name": last_name,
@@ -40,15 +46,33 @@ def extract_relevant_data(raw_data):
         }
 
         cleanedData.append(filtered_detail)
-
     return cleanedData
 
 
-def cleanData(raw_data, imageId):
-    # to make sure that raw_data I am supplying is a dict and not a list
-    # print(f"raw data type: {type(raw_data)}")
-    # print(raw_data)
-    # Save raw data to JSON file for inspection/debugging
-    data = extract_relevant_data(raw_data)
+def cleanData(processed_data, imageId):
+    data_frame = pd.json_normalize(
+        processed_data,
+        meta=['first_name', 'last_name', 'linkedin_url', 'title', 'email'],
+        record_path='employment_history',
+        errors='ignore'
+    )
+
+    data_frame_reindexed_cols = data_frame.reindex(
+        columns=[
+            'first_name',
+            'last_name',
+            'linkedin_url',
+            'organization_name',
+            'title',
+            'email',
+            'last_job',
+            'last_job_title'])
+    file_name = f"{imageId}"
+    data_frame_reindexed_cols.to_excel(
+        f"enrichedData/{file_name}.xlsx", index=False)
+    print(f"saved as {file_name}.xlsx in dir: enrichedData")
+
+    records = data_frame_reindexed_cols.to_dict(orient="records")
     with open(f"enrichedData/{imageId}_enrichedData.json", 'w') as f:
-        json.dump(data, f, indent=4)
+        json.dump(records, f, indent=4)
+    print(f"saved as {file_name}_enrichedData.json in dir: enrichedData")
