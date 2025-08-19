727afd183324584b55062c408b2d360d9671a7be
Femto-0 <arits.king74@gmail.com>
2025-08-19 10:22:18 -0500

    modified the main function such that all the images present in the images dir get processed when the script runs

--- source/main.py
+++ source/main.py
@@ -7,17 +7,24 @@ model = "llama3.2"
 
 def main():
     # write the name of your image here without the extension. For example: for "imageA.jpg", just write "imageA"
-    image = 'IMG_5293'
-    imageInfo = pi.extract_text_from_image('images/'+image+'.PNG')
-    ollama_response = qo.query_ollama(imageInfo, model)
-    # get raw data from ollama
-    raw_data = qa.query_apollo(ollama_response)
-    # clean the raw data and filter out the info that we want.
-    # As of now, the filter data contains:
-    # ['first_name','last_name','linkedin_url','organization_name','title','email','last_job','last_job_title']
-    clean_data = ced.extract_relevant_data(raw_data)
-    # save the clean data in a json and xlsx file
-    ced.cleanData(clean_data, image)
+    try:
+        num = 5295
+        finalNum = 5296
+        while num <= finalNum:
+            image = f"IMG_{num}"
+            imageInfo = pi.extract_text_from_image('images/'+image+'.PNG')
+            ollama_response = qo.query_ollama(imageInfo, model)
+            # get raw data from ollama
+            raw_data = qa.query_apollo(ollama_response)
+            # clean the raw data and filter out the info that we want.
+            # As of now, the filter data contains:
+            # ['first_name','last_name','linkedin_url','organization_name','title','email','last_job','last_job_title']
+            clean_data = ced.extract_relevant_data(raw_data)
+            # save the clean data in a json and xlsx file
+            ced.cleanData(clean_data, image)
+            num += 1
+    except Exception as e:
+        print(f"Error {e}")
 
 
 if __name__ == "__main__":
